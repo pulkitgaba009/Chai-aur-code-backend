43fe136f6c9d591118aca86147b8661dbc8ce1fd
pulkitgaba009 <pulkitgaba009@gmail.com>
2025-08-29 19:13:15 +0530

    user model and video model with hooks and jwt

---
+++ Project/backend/src/models/video.model.js
@@ -0,0 +1,44 @@
+import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    title: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: Number,
+      required: true,
+    },
+    views: {
+      type: Number,
+      required: true,
+    },
+    isPublished: {
+      type: Boolean,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+videoSchema.plugin(mongooseAggregatePaginate)
+
+export const Video = mongoose.model("Video", videoSchema);
--- Project/backend/src/utils/asyncHandler.js
+++ Project/backend/src/utils/asyncHandler.js
@@ -1,7 +1,8 @@
 const asyncHandler = (requestHandeler) => {
-    (req,res,next) => {
-        Promise.resolve(requestHandeler(req,res,next))
-        .catch((err)=>next(err))
+    return (req,res,next) => {
+        Promise
+        .resolve(requestHandeler(req,res,next))
+        .catch((err)=>next(err));
     }
 }
 
